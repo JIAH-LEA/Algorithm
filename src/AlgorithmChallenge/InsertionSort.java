9af43cdb8037c590e71fcff7c1ab6806cc3267b9
JIAH LEE <66239292+JIAH-LEA@users.noreply.github.com>
2022-02-11 10:23:08 +0000

    [AlgorithmChallenge] Insertion Sort

---
+++ src/AlgorithmChallenge/InsertionSort.java
@@ -0,0 +1,38 @@
+package AlgorithmChallenge;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class InsertionSort {
+    public ArrayList<Integer> insertionSort (ArrayList<Integer> dataList){
+
+        for(int index = 0; index < dataList.size()-1; index++){
+
+            for(int index2 = index+1; index2 >0; index2--){
+
+                if(dataList.get(index2) < dataList.get(index2-1))
+                    Collections.swap(dataList, index2, index2-1);
+                else
+                    break;
+
+            }
+            System.out.println(dataList);
+
+        }
+        return dataList;
+
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> testData = new ArrayList<Integer>();
+
+        for (int i = 0; i < 10; i++)
+            testData.add((int) (Math.random() * 100));
+
+        InsertionSort iSort = new InsertionSort();
+
+        iSort.insertionSort(testData);
+
+    }
+}
