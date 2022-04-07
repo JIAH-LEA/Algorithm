6821df885ab869fd633dcf340a81eca1d95d2005
JIAH LEE <66239292+JIAH-LEA@users.noreply.github.com>
2022-04-07 23:50:35 +0900

    [BOJ] 2407 - 조합(DP, 수학, 큰 수 연산)

---
+++ src/BOJ/BOJ2407.java
@@ -0,0 +1,40 @@
+package BOJ;
+
+// 2407 - 조합
+// 실버3 - 임의정밀도, 큰 수 연산, DP, 조합론, 수학
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BOJ2407 {
+
+    static BigInteger[][] dp = new BigInteger[101][101]; // 1-100까지 인덱스 사용
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        dynamicFunc( n , m );
+
+    }
+    // 재귀 방식 시간초과 > 파스칼 삼각형 이용
+    static void dynamicFunc(int n, int m) {
+
+        dp[1][0] = dp[1][1] = BigInteger.ONE; // 1C0 = 1C1 = 1
+        for( int i = 0; i < 101; i++ ) {
+            for( int j = 0; j <= i; j++ ) {
+                if( i == j || j == 0 )
+                    dp[i][j] = BigInteger.ONE; // iC0 = iCi = 1
+                else
+                    dp[i][j] = dp[i-1][j].add(dp[i-1][j-1]); // iCj = i-1Cj + i-1Cj-1
+            }
+        }
+        System.out.println(dp[n][m]);
+    }
+}
