19fdbf4f2d4662f4d9924eb26154bc96b884cc46
JIAH LEE <66239292+JIAH-LEA@users.noreply.github.com>
2022-04-08 00:00:24 +0900

    [BOJ] 2407 - 조합(DP, 수학, 큰 수 연산)

--- src/BOJ/BOJ2407.java
+++ src/BOJ/BOJ2407.java
@@ -20,21 +20,20 @@ public class BOJ2407 {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        dynamicFunc( n , m );
-
+        dynamicFunc(); // 해당 조합 범위의 파스칼 삼각형 만들기
+        System.out.println(dp[n][m]); // 원하는 값 추출
     }
     // 재귀 방식 시간초과 > 파스칼 삼각형 이용
-    static void dynamicFunc(int n, int m) {
+    static void dynamicFunc() {
 
         dp[1][0] = dp[1][1] = BigInteger.ONE; // 1C0 = 1C1 = 1
         for( int i = 0; i < 101; i++ ) {
-            for( int j = 0; j <= i; j++ ) {
-                if( i == j || j == 0 )
+            for (int j = 0; j <= i; j++) {
+                if (i == j || j == 0)
                     dp[i][j] = BigInteger.ONE; // iC0 = iCi = 1
                 else
-                    dp[i][j] = dp[i-1][j].add(dp[i-1][j-1]); // iCj = i-1Cj + i-1Cj-1
+                    dp[i][j] = dp[i - 1][j].add(dp[i - 1][j - 1]); // iCj = i-1Cj + i-1Cj-1
             }
         }
-        System.out.println(dp[n][m]);
     }
 }
